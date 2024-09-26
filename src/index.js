9eb8b39ab999dd456272b425f1f9944bd4a5ed22
graciaollo <olloclemencia@gmail.com>
2024-09-26 09:01:05 -0700

    sintegrations

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {AuthProvider} from './components/Context/AuthContext.js'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
